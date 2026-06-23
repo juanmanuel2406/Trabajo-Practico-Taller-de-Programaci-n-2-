3b497d9df1c5532844c039d59347775711b7b4a1
Juan Manuel <trabajo@test.com>
2026-06-23 16:52:53 -0300

    Fix: POST /usuarios sin autenticacion (como el modelo del profe)

--- src/routes/usuario.router.js
+++ src/routes/usuario.router.js
@@ -2,11 +2,9 @@ const express = require('express')
 const { crearUsuario, login } = require('../controllers/usuario.controller')
 const { validator } = require('../middlewares/validatorHandler')
 const { loginSchema, postUsuarioSchema } = require('../schemas/usuario.schema')
-const { checkAdmin } = require('../middlewares/secure')
-
 const usuarioRouter = express.Router()
 
-usuarioRouter.post('/', checkAdmin(), validator(postUsuarioSchema, 'body'), crearUsuario)
+usuarioRouter.post('/', validator(postUsuarioSchema, 'body'), crearUsuario)
 usuarioRouter.post('/login', validator(loginSchema, 'body'), login)
 
 module.exports = usuarioRouter
